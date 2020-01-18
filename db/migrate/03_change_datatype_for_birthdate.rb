d544d571fac3a0cd71d3fac60eac174a2371dbcc
Michael Carroll <carrollm2@gmail.com>
2020-01-18 01:34:02 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -2,7 +2,7 @@ class ChangeDatatypeForBirthdate< ActiveRecord::Migration[5.2]
   def change
 
     change_column :students, :grade, :integer
-    change_column :students, :name, :text
+    change_column :students, :birthdate, :datetime
 
   end
 
