ccd91198872afad1cde745b42f3078e42a3a6978
Michael Carroll <carrollm2@gmail.com>
2020-01-18 01:08:59 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents
   def change
-    add_column :grade_and_birthdate do |t|
+    add_column :grade_and_birthdates do |t|
       t.integer :grade
       t.string :birthdate
     end
